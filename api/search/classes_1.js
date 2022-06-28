a361eb8ae8311e91d7d2cafac3d1c5bf9ba5981f
jviotti <jviotti@users.noreply.github.com>
2022-06-28 00:40:29 +0000

    deploy: a2860edcd6e263a981ae3cab8a3830926319a463

--- api/search/classes_1.js
+++ api/search/classes_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['booleanasenum_46',['BooleanAsEnum',['../classsourcemeta_1_1jsonbinpack_1_1canonicalizer_1_1rules_1_1_boolean_as_enum.html',1,'sourcemeta::jsonbinpack::canonicalizer::rules']]],
   ['booleanschema_47',['BooleanSchema',['../classsourcemeta_1_1jsonbinpack_1_1canonicalizer_1_1rules_1_1_boolean_schema.html',1,'sourcemeta::jsonbinpack::canonicalizer::rules']]],
-  ['bundle_48',['Bundle',['../classsourcemeta_1_1jsonbinpack_1_1canonicalizer_1_1_bundle.html',1,'sourcemeta::jsonbinpack::canonicalizer']]]
+  ['bundle_48',['Bundle',['../classsourcemeta_1_1alterschema_1_1_bundle.html',1,'sourcemeta::alterschema']]]
 ];
--- api/search/classes_a.js
+++ api/search/classes_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['rule_85',['Rule',['../classsourcemeta_1_1jsonbinpack_1_1canonicalizer_1_1_rule.html',1,'sourcemeta::jsonbinpack::canonicalizer']]]
+  ['rule_85',['Rule',['../classsourcemeta_1_1alterschema_1_1_rule.html',1,'sourcemeta::alterschema']]]
 ];
